59c479c003939e72059b102aa7f7a7c4a6dd4311
Saransh Bangar <saranshbangad@gmail.com>
2024-07-18 08:00:59 +0530

    Resolved build error - 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import "./App.css";
-import { API_KEY } from "../variables";
 
 function App() {
   const [prompt, setPrompt] = useState("");
@@ -14,6 +13,7 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const API_KEY = "AIzaSyCUfopTNKCnt2pjEORnLmp5HomPZghvvMA";
     const genAI = new GoogleGenerativeAI(API_KEY);
     const updatedPrompt = `Remember, your name is BangerAI and you are acting as an AI Writing Assistant. I will provide you with a prompt, the desired word limit, and the number of paragraphs to generate. The prompt can be anything random, and you should create an essay based on it. The word count should be approximately close to the specified limit. After every paragraph, include an extra line for clarity. Please note that formatting like bold, italics, bullet points, or emojis is not allowed. If no prompt is provided, respond with 'Please provide a prompt.' Currently, you are speaking to the developer, but your responses will be read by the general public, who may not be technically savvy. Remember to include the extra line. Got it? Here is your prompt -> ${prompt}, the word limit -> ${wordLimit}, and the paragraph number -> ${paragraph}.`
 
@@ -60,10 +60,10 @@ function App() {
     <section className="min-h-[100vh] max-h-fit flex justify-center bg-slate-900">
       <div className="w-[75%] flex flex-col">
         <div className="">
-          <h1 className="font-sans text-center py-8 text-4xl font-extrabold  tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Write an essay using just a single word!</h1>
+          <h1 className="py-8 font-sans text-4xl font-extrabold tracking-tight text-center text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Write an essay using just a single word!</h1>
           <h3 className="font-sans text-center mt-2 text-2xl font-medium  tracking-tight text-gray-900 md:text-[24px] lg:text-3xl dark:text-white/60">Unleash Your Creativity : Craft an Entire Essay with Just One Powerful Word</h3>
         </div>
-        <div className="flex justify-center items-center my-12">
+        <div className="flex items-center justify-center my-12">
           <form onSubmit={handleSubmit} className="w-[80%] flex flex-col gap-8">
             <div className="flex flex-row justify-evenly max-md:flex-col max-md:gap-4">
               <input
@@ -91,7 +91,7 @@ function App() {
                 className="w-[30%] max-md:w-full h-10 placeholder:text-black/60 rounded-lg text-xl p-1 pl-2 focus:outline-none"
               />
             </div>
-            <div className="flex justify-center items-center">
+            <div className="flex items-center justify-center">
               <button type="submit" className="w-[100px] h-[45px] text-xl bg-slate-600 rounded-full transition-all duration-150 ease-in-out hover:bg-slate-500 hover:-translate-y-[2px] hover:scale-[1.05]">Submit</button>
             </div>
           </form>
@@ -115,7 +115,7 @@ function App() {
         {
           text
             &&
-          <div className="flex justify-center items-center my-8">
+          <div className="flex items-center justify-center my-8">
             <div className="w-[30%] flex justify-between items-center max-md:w-full max-md:flex-col max-md:gap-4">
               <button onClick={handleCopy} className="w-[40%] max-md:w-[50%] h-[45px] text-xl bg-slate-600 rounded-full transition-all duration-150 ease-in-out hover:bg-slate-500 hover:-translate-y-[2px] hover:scale-[1.05] focus:outline-none">Copy</button>
               <button onClick={handleDownload} className="w-[40%] max-md:w-[50%] h-[45px] text-xl bg-slate-600 rounded-full transition-all duration-150 ease-in-out hover:bg-slate-500 hover:-translate-y-[2px] hover:scale-[1.05] focus:outline-none">Download</button>
